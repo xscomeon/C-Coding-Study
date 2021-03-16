68a890ffee9df0aa858037b523d69d301dfcc744
xscomeon <lxs953779555@163.com>
2021-03-16 13:18:41 +0800

    谭浩强C语言第一章

---
+++ 谭浩强C语言课后习题/习题1/习题1/test1.c
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS 1  
+
+//1.程序：一组计算机能够识别和执行的指令集合
+  //程序设计：从确定任务到得到结果、写出文档的全过程
+
+//2.计算机语言解决了人与计算机交流的语言问题
+//  高级语言：C C++ JAVA  Python 
+//			高级语言的数据类型比汇编语言更加丰富
+//			高级语言更加亲近自然语言，容易掌握
+//			高级语言编写的代码需要经过编译才能变成程序
+//			高级语言与具体的计算机结构没有很强的关系，机器语言与机器有较强的关系
+
+//3. 源程序  目标程序(.obj)  可执行程序(.exe)
+	//程序编辑  程序编译  程序链接：将编译后的目标模块连接起来
+	//程序  程序模块   程序文件
+
+//4. 函数  主函数  被调用函数  库函数
+
+//5.程序调试  程序测试
+
+//6.
+//#include <stdio.h>
+//int main()
+//{
+//	printf("hello world\n");
+//	printf("%s\n", "Hello World");
+//	return 0;
+//}
+
+//7.
+//#include <stdio.h>
+//int main()
+//{
+//	printf("*****\n");
+//	printf("  *****\n");
+//	printf("    *****\n");
+//	printf("      *****\n");
+//	return 0;
+//}
+
+//8.
+//#include <stdio.h>
+//int main()
+//{
+//	int a, b, c;
+//	printf("Please input 3 numbers:");
+//	scanf("%d%d%d", &a, &b, &c);
+//	int max;
+//	if (a > b)
+//	{
+//		max = a;
+//	}
+//	else
+//	{
+//		max = b;
+//	}
+//	if (c > max)
+//	{
+//		max = c;
+//	}
+//
+//	printf("max is %d\n", max);
+//
+//	return 0;
+//}
